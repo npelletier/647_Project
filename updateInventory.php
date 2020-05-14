f2ea839db8fbee15f0adc8e3f2baeff550c3ca4d
Earving8 <47326947+Earving8@users.noreply.github.com>
2020-05-14 18:45:32 -0500

    updateinventory makeover

--- updateInventory.php
+++ updateInventory.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
+<title>Edit Inventory</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles.css">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Oswald:wght@400;700&family=Roboto:ital,wght@0,700;1,400&display=swap" rel="stylesheet">
+<!--[if lt IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+    <div id="wrapper">
+        <div class="section">
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "nathanpelletier", "ooRao3En", "nathanpelletier");
 
@@ -18,17 +37,7 @@ $add_names = $_POST["add"];
 $add_values = $_POST["addValue"];
 $existingQuery = "SELECT item_name, amount FROM `HAS` WHERE player_name = '$playername' and character_name = '$charname'";
 
-/* display inventory */
-echo "Initial Inventory<br>";
-if($existing = $mysqli->query($existingQuery)){
-    while($row = $existing->fetch_assoc()){
-        echo $row['item_name']." | ".$row['amount']."<br>";
-    }
-}else{
-    echo "Error";
-}
-
-echo "<br>Removing...<br>";
+echo "<h1>Following changes were made to inventory...</h1>";
 
 if(count($removed_names) > 0){
     for($i = 0; $i < count($removed_names); $i++){
@@ -39,7 +48,7 @@ if(count($removed_names) > 0){
                 /* Need to remove item from table */
                 $update = "DELETE FROM `HAS` WHERE player_name = '$playername' and character_name = '$charname' and item_name ='".$removed_names[$i]."'";
                 if($update_status = $mysqli->query($update)){
-                    echo "-".$removed_values[$i]." ".$removed_names[$i]." removed from database<br>";
+                    echo "<p>-".$removed_values[$i]." ".$removed_names[$i]." removed from database</p>";
                 }else{
                     echo "Error in updating database<br>";
                 }
@@ -48,7 +57,7 @@ if(count($removed_names) > 0){
                 $newAmount = $row['amount'] - $removed_values[$i];
                 $update = "UPDATE `HAS` SET amount = $newAmount WHERE player_name = '$playername' and character_name = '$charname' and item_name ='".$removed_names[$i]."'";
                 if($update_status = $mysqli->query($update)){
-                    echo "-".$removed_values[$i]." ".$removed_names[$i]." ".$row['amount']." -> ".$newAmount."<br> ";
+                    echo "<p>-".$removed_values[$i]." ".$removed_names[$i]." ".$row['amount']." -> ".$newAmount."</p> ";
                 }else{
                     echo "Error in updating detabase<br>";
                 }
@@ -64,7 +73,6 @@ if(count($removed_names) > 0){
 }
 
 
-echo "<br>Adding...<br>";
 if(count($add_names) > 0){
     for($i = 0; $i < count($add_names); $i++){
 
@@ -98,21 +106,14 @@ if(count($add_names) > 0){
     echo "Nothing added <br>";
 }
 
-$existingQuery = "SELECT * FROM `HAS` WHERE player_name = '$playername' and character_name = '$charname'";
-
-/* display inventory */
-echo "<br>Resulting Inventory<br>";
-if($existing = $mysqli->query($existingQuery)){
-    while($row = $existing->fetch_assoc()){
-        echo $row['item_name']." | ".$row['amount']."<br>";
-    }
-}else{
-    echo "Error";
-}
-
-echo "<br><a href='inventory.php'>Return</a>";
+echo "<br><a class='button' href='inventory.php'>Return</a>";
 
 
 $mysqli->close();
 
 ?>
+        </div>
+    </div>
+
+</body>
+</html>
