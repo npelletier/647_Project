3c1e97b4425112aedd6884b00df268cb7e12d3a0
Earving8 <47326947+Earving8@users.noreply.github.com>
2020-05-13 20:22:49 -0500

    added more information about character

--- character.php
+++ character.php
@@ -35,11 +35,21 @@
     </nav>
     <div id="wrapper">
             <?php
-                $query = "SELECT description FROM `CHARACTER` WHERE username = '$playername' and name = '$charname'";
+                $query = "SELECT * FROM `CHARACTER` WHERE username = '$playername' and name = '$charname'";
     
                 if($result = $mysqli->query($query)){
                     while($row = $result->fetch_assoc()){
-                        echo "<h1>".$playername." | ".$charname."</h1><p> ".$row['description']."</p>";
+                        echo 
+                            "<p>Character Name</p><h1 class ='botMargin'>".$row['name']."</h1>".
+                            "<p>Class</p><h1 class ='botMargin'>".$row['class']."</h1>".
+                            "<p>Background</p><h1 class ='botMargin'>".$row['background']."</h1>".
+                            "<p>Player Name</p><h1 class ='botMargin'>".$row['username']."</h1>".
+                            "<p>Race</p><h1 class ='botMargin'>".$row['race']."</h1>".
+                            "<p>Alignment</p><h1 class ='botMargin'>".$row['alignment']."</h1>".
+                            "<p>Experience Points</p><h1 class ='botMargin'>".$row['xp']."</h1>".
+                            "<p>Description</p><h1 class ='botMargin'>".$row['description']."</h1>".
+                            "<p>HP</p><h1 class ='botMargin'>".$row['currenthp']."/".$row['maximumhp']."</h1>".
+                            "<p>Speed</p><h1 class ='botMargin'>".$row['speed']."</h1>";
                     }
                 }else{
                     echo "Error";
