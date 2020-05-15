fa52474c494516a1ef822108013fd3ace56b557e
Nathan Pelletier <nathan.a.pelletier@gmail.com>
2020-05-15 02:43:36 -0500

    Everything is awesome!!!!

--- editCharacter.php
+++ editCharacter.php
@@ -44,7 +44,7 @@
                 $query = "SELECT * FROM `CHARACTER` WHERE username = '$playername' and name = '$charname'";
 
                 $username = $playername;
-                $name = $charname;
+                $name = "CharName";
                 $class = "random";
                 $alignment = "neutral";
                 $xp = 0;
@@ -74,17 +74,17 @@
                 }
 
                 echo"
-                    <label>User Name</label><br><input type='text' name='username' value='$username'><br>
-                    <label>Character Name</label><br><input type='text' name='name' value='$name'><br>
-                    <label>Class</label><br><input type='text' name='class' value='$class'><br>
-                    <label>Alignment</label><br><input type='text' name='alignment' value='$alignment'><br>
-                    <label>Experience</label><br><input type='text' name='xp' value='$xp'><br>
-                    <label>Race</label><br><input type='text' name='race' value='$race'><br>
+                    <label>User Name</label><br><input required type='text' name='username' value='$username'><br>
+                    <label>Character Name</label><br><input required type='text' name='name' value='$name'><br>
+                    <label>Class</label><br><input required type='text' name='class' value='$class'><br>
+                    <label>Alignment</label><br><input required type='text' name='alignment' value='$alignment'><br>
+                    <label>Experience</label><br><input required type='text' name='xp' value='$xp'><br>
+                    <label>Race</label><br><input required type='text' name='race' value='$race'><br>
                     <label>Description</label><br><input type='text' name='description' value='$description'><br>
-                    <label>Background</label><br><input type='text' name='background' value='$background'><br>
-                    <label>Current HP</label><br><input type='text' name='currenthp' value='$currenthp'><br>
-                    <label>Maximum HP</label><br><input type='text' name='maximumhp' value='$maximumhp'><br>
-                    <label>Speed</label><br><input type='text' name='speed' value='$speed'><br>";
+                    <label>Background</label><br><input required type='text' name='background' value='$background'><br>
+                    <label>Current HP</label><br><input required type='text' name='currenthp' value='$currenthp'><br>
+                    <label>Maximum HP</label><br><input required type='text' name='maximumhp' value='$maximumhp'><br>
+                    <label>Speed</label><br><input required type='text' name='speed' value='$speed'><br>";
 
             ?>
             <input type='submit' value="Submit">
---
+++ updateCharacter.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+$curUser=$_SESSION['playername'];
+$curChar=$_SESSION['charname'];
+
+$username = $_POST['username'];
+$name = $_POST['name'];
+$class = $_POST['class'];
+$alignment = $_POST['alignment'];
+$xp = $_POST['xp'];
+$race = $_POST['race'];
+$description = $_POST['description'];
+$background = $_POST['background'];
+$currenthp = $_POST['currenthp'];
+$maximumhp = $_POST['maximumhp'];
+$speed = $_POST['speed'];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "nathanpelletier", "ooRao3En", "nathanpelletier");
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+
+if(($_SESSION['edit'])=='false'){
+  $sql = "INSERT INTO `CHARACTER`(`username`, `name`, `class`, `alignment`, `xp`, `race`, `description`, `background`, `currenthp`, `maximumhp`, `speed`) VALUES ('$username', '$name', '$class', '$alignment', $xp, '$race', '$description', '$background', $currenthp, $maximumhp, $speed)";
+  $results = $mysqli->query($sql);
+  if($results){
+    $sql = "INSERT INTO `HAS`(`player_name`, `character_name`, `item_name`, `amount`, `equipped`) VALUES ('$username','$name','Copper Piece',100,0)";
+    $results = $mysqli->query($sql);
+    $mysqli->close();
+    header("Location: Dashboard.php");
+    exit();
+  }
+}
+else{
+  $sql = "UPDATE `CHARACTER` SET class='$class', alignment='$alignment', xp=$xp, race='$race', description='$description', background='$background', currenthp=$currenthp, maximumhp=$maximumhp, speed=$speed WHERE username='$curUser' AND name='$curChar'";
+  $results = $mysqli->query($sql);
+  if($results){
+    $mysqli->close();
+    header("Location: Dashboard.php");
+    exit();
+  }
+}
+
+?>
